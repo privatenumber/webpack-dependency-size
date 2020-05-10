3c8ddfe98c57d6e735326fc1d858ba34936a4c58
Hiroki Osame <osame@squareup.com>
2020-05-10 01:23:46 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# webpack-dependency-size
+<h1>
+	👩‍🔬 Webpack Dependency Size Plugin
+	<a href="https://npm.im/webpack-dependency-size"><img src="https://badgen.net/npm/v/webpack-dependency-size"></a>
+	<a href="https://npm.im/webpack-dependency-size"><img src="https://badgen.net/npm/dm/webpack-dependency-size"></a>
+	<a href="https://packagephobia.now.sh/result?p=webpack-dependency-size"><img src="https://packagephobia.now.sh/badge?p=webpack-dependency-size"></a>
+</h1>
+
 Webpack plugin to get an overview of bundled dependencies and their size
+
+## :rocket: Install
+```sh
+npm i webpack-dependency-size
+```
+
---
+++ index.js
@@ -0,0 +1,126 @@
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+const filesize = require('filesize');
+const gzipSize = require('gzip-size');
+
+// Strips loaders (!) and " + n modules" suffix
+const getFilepath = name => name && name.split('!').pop().replace(/ \+.+$/, '');
+
+// Gets the dependency name
+const ptrn = /^(.*node_modules\/(@[^\/]+\/)?[^\/]+)/;
+const getDepName = filepath => _.head(filepath.match(ptrn));
+
+const statsOpts = {
+	assets: false,
+	builtAt: false,
+	moduleAssets: false,
+	moduleTrace: false,
+	cachedAssets: false,
+	children: false,
+	chunks: false,
+	chunkGroups: false,
+	chunkModules: false,
+	chunkRootModules: false,
+	chunkOrigins: false,
+	source: false,
+	performance: false,
+	providedExports: false,
+	errors: false,
+	errorDetails: false,
+	errorStack: false,
+	entrypoints: false,
+	warnings: false,
+	outputPath: false,
+	hash: false,
+	publicPath: false,
+	timings: false,
+	version: false,
+	logging: 'none',
+};
+
+class DependencySizePlugin {
+
+	constructor({
+		outputPath = 'dependency-size.json',
+		gzip = false,
+		indent = '\t',
+	} = {}) {
+		this.outputPath = outputPath;
+		this.gzip = gzip;
+		this.indent = indent;
+	}
+
+	apply(compiler) {
+		this.compiler = compiler;
+		const analyzeStats = this.analyzeStats.bind(this);
+
+		if (compiler.hooks) {
+			compiler.hooks.done.tapAsync('webpack-dependency-size', analyzeStats);
+		} else {
+			compiler.plugin('done', analyzeStats);
+		}
+	}
+
+	getDependencyModules({ modules }) {
+		return modules
+			.map((m) => ({
+				filepath: getFilepath(m.name),
+				size: this.gzip ? gzipSize.sync(m.source) : m.size,
+				reasons: _.uniq(m.reasons.map(r => getFilepath(r.moduleName))).sort(),
+			}))
+			.filter((m) => m.filepath.startsWith('./node_modules/'));
+	}
+
+	analyzeStats(stats, cb = _.noop) {
+		const statsJson = stats.toJson({
+			...statsOpts,
+			source: this.gzip,
+		});
+
+		let deps = this.getDependencyModules(statsJson)
+			.reduce((deps, m) => {
+				const depName = getDepName(m.filepath);
+
+				if (!deps[depName]) {
+					deps[depName] = {
+						size: 0,
+						files: [],
+					};
+				}
+
+				deps[depName].size += m.size;
+				deps[depName].files.push(m);
+
+				return deps;
+			}, {});
+
+		deps = _(deps)
+			.toPairs()
+			.orderBy(['1.size'], ['desc'])
+			.map((dep) => {
+				dep[1].size = filesize(dep[1].size);
+				dep[1].files
+					.sort((a, b) => b.size - a.size)
+					.forEach((f) => {
+						f.size = filesize(f.size);
+					});
+
+				return dep;
+			})
+			.fromPairs()
+			.value();
+
+		this.writeData(deps, cb);
+	}
+
+	writeData(data, cb) {
+		fs.writeFile(
+			path.resolve(this.compiler.outputPath, this.outputPath),
+			JSON.stringify(data, null, this.indent),
+			cb
+		);
+	}
+}
+
+module.exports = DependencySizePlugin;
